ef14bd599f234863ca939eeae31e7394fec2a301
SAA DEV 06 <victorgabrielcorreasilva0@gmail.com>
2026-03-12 20:40:39 -0400

    primeiro commit

---
+++ src/main/java/com/example/compra_programada/CompraProgramadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.compra_programada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraProgramadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraProgramadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/compra_programada/CompraProgramadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compra_programada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraProgramadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
